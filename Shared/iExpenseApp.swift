aa06fa3f07fb6694f3156d73ad665daee906c567
Erik Mikac <mikacew@gmail.com>
2021-03-14 19:35:07 -0400

    Initial Commit

---
+++ Shared/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  Shared
+//
+//  Created by Erik Mikac on 3/14/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
